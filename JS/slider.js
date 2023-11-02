e74a9f374870166667bd7b8008e2dfa7f9493e88
marsegovia <marsegovia@frba.utn.edu.ar>
2023-11-01 22:51:13 -0300

    JS SLider listo, faltan letras y otra page

--- JS/slider.js
+++ JS/slider.js
@@ -1,45 +1,60 @@
 var slides = document.querySelectorAll('.slide');
 var currentSlide = 0;
+var interval;
 
 function showSlide(n) {
     slides[currentSlide].classList.remove('active');
     currentSlide = (n + slides.length) % slides.length;
     slides[currentSlide].classList.add('active');
+    updateIndicators();
 }
 
 function changeSlide(n) {
     showSlide(currentSlide + n);
 }
 
-document.querySelector('.prev').addEventListener('click', function() {
-    changeSlide(-1);
-});
+function createIndicators() {
+    var indicatorsContainer = document.querySelector('.carousel-indicators');
 
-document.querySelector('.next').addEventListener('click', function() {
-    changeSlide(1);
-});
+    for (var i = 0; i < 0; i++) {
+        var indicator = document.createElement('button');
+        if (i === 0) {
+            indicator.classList.add('active');
+            indicator.setAttribute('aria-current', 'true');
+        }
 
-showSlide(currentSlide);
+        indicator.addEventListener('click', function() {
+            var slideIndex = parseInt(this.getAttribute('data-bs-slide-to'));
+            showSlide(slideIndex);
+        });
 
-var slides = document.querySelectorAll('.slide');
-var currentSlide = 0;
-
-function showSlide(n) {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (n + slides.length) % slides.length;
-    slides[currentSlide].classList.add('active');
+        indicatorsContainer.appendChild(indicator);
+    }
 }
 
-function changeSlide(n) {
-    showSlide(currentSlide + n);
+function startSlideInterval() {
+    interval = setInterval(function() {
+        changeSlide(1);
+    }, 4000); // Cambia la diapositiva cada 5 segundos (5000 milisegundos)
 }
 
-document.querySelector('.prev').addEventListener('click', function() {
-    changeSlide(-1);
-});
+function updateIndicators() {
+    var indicators = document.querySelectorAll('.carousel-indicators button');
+    indicators.forEach(function(indicator, index) {
+        if (index === currentSlide) {
+            indicator.classList.add('active');
+            indicator.setAttribute('aria-current', 'true');
+        } else {
+            indicator.classList.remove('active');
+            indicator.removeAttribute('aria-current');
+        }
+    });
+}
 
-document.querySelector('.next').addEventListener('click', function() {
-    changeSlide(1);
-});
+function initializeCarousel() {
+    createIndicators();
+    showSlide(currentSlide);
+    startSlideInterval(); // Comienza el cambio automático de diapositivas al cargar la página
+}
 
-showSlide(currentSlide);
+initializeCarousel();
